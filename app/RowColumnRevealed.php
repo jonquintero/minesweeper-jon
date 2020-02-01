04e792f97e4e306aaf3a373db211735f4e3b3202
Jonathan Quintero <jonquintero@hotmail.com>
2020-02-01 19:32:46 -0400

    creations models

---
+++ app/RowColumnRevealed.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RowColumnRevealed extends Model
+{
+    protected $table = 'rows_columns_revealed';
+}
---
+++ app/SetRandomMine.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SetRandomMine extends Model
+{
+    protected $table = 'set_random_mines';
+}
