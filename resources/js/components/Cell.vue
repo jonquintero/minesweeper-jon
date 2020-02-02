0a37cea43836f55dc32a242c8f096dad5707d82f
Jonathan Quintero <jonquintero@hotmail.com>
2020-02-01 20:53:22 -0400

    new component vue

---
+++ resources/js/components/Cell.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="cell"
+        :class="classes.join(' ')"
+        @contextmenu.prevent="flag"
+        @click="flip"
+    >
+        <template v-if="item.status === statuses.OPEN">
+            <template v-if="item.isEmpty"></template>
+
+            <template v-else-if="! item.isMine && item.adjacentMines != 0">
+                {{ item.adjacentMines }}
+            </template>
+
+            <template v-else-if="item.isMine">
+                <span class="is-mine"> * ! </span>
+            </template>
+        </template>
+
+        <template v-else-if="item.status === statuses.CHECKED"> ? </template>
+    </div>
+</template>
+
+<script>
+
+
+
+    import EventBus from './eventBus.js';
+
+    export default {
+
+        props : {
+
+            item: {
+                type: Object,
+                required: true
+            },
+
+        },
+
+        data() {
+            return {
+                classes: ['facedown'],
+                statuses: {
+                    FACEDOWN: 'facedown',
+                    OPEN : 'open',
+                    FLAGGED : 'flagged',
+                    CHECKED : 'checked'
+                }
+            }
+        },
+
+        computed: {
+            status() {
+                return this.item.status;
+            }
+        },
+
+        watch: {
+            status(newStatus, oldStatus) {
+                if (newStatus === this.statuses.CHECKED) {
+                    this.classes = [
+                        this.statuses.FACEDOWN,
+                        this.statuses.CHECKED,
+                    ];
+                } else if (newStatus === this.statuses.FLAGGED) {
+                    this.classes = [
+                        this.statuses.FACEDOWN,
+                        this.statuses.FLAGGED,
+                    ];
+                } else if (newStatus === this.statuses.OPEN) {
+                    this.classes = [this.statuses.OPEN, 'invalidate'];
+                } else {
+                    this.classes = [this.statuses.FACEDOWN];
+                }
+            },
+
+        },
+
+        methods: {
+            flip() {
+                if ([this.statuses.OPEN, this.statuses.FLAGGED].includes(this.item.status)) {
+                    return;
+                }
+
+                this.item.status = this.statuses.OPEN;
+
+                if (this.item.isMine) {
+                    EventBus.$emit('mineFound');
+                    alert('You lost');
+
+                   setTimeout(this.gohome, 3000);
+
+
+                } else {
+                    EventBus.$emit('flippedCell', this.item);
+                }
+            },
+
+            flag() {
+                if (this.item.status == this.statuses.OPEN) {
+                    return;
+                }
+
+                if (this.item.status === this.statuses.FACEDOWN) {
+                    this.item.status = this.statuses.CHECKED
+                } else if (this.item.status === this.statuses.CHECKED) {
+                    this.item.status = this.statuses.FLAGGED
+                } else {
+                    this.item.status = this.statuses.FACEDOWN;
+                }
+            },
+
+            gohome(){
+                this.$router.push({name: 'home'});
+                location.reload()
+            }
+
+
+        }
+    }
+</script>
+
+<style>
+    .cell {
+        width: 50px;
+        height: 50px;
+        margin: 2px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 200%;
+        font-weight: bolder;
+        text-shadow: 2px 2px rgba(52, 58, 64, 0.5);
+    }
+
+    .facedown {
+        background-color: lightskyblue;
+    }
+
+    .open {
+        background-color: lightyellow;
+    }
+
+    .flagged {
+        background-color: red;
+    }
+
+    .checked {
+        color: black;
+    }
+
+    .is-mine {
+        color: red;
+    }
+
+    .invalidate {
+        pointer-events: none;
+    }
+</style>
